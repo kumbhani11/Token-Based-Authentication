144d8774adcfc33dfc820600e06548d6c31cebb7
Devang Kumbhani <51017576+kumbhani11@users.noreply.github.com>
2021-09-30 22:47:32 +0530

    Update models.py

--- project/application/models.py
+++ project/application/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+#model
+
 class Users(models.Model):
     fullname = models.CharField(max_length=255, blank=True)
     email = models.CharField(max_length=255, blank=False, unique=True)
